8883d0c4ddbdf6cff0125517cdb015c25d5ac7b9
Lianca <lianca0309@gmail.com>
2025-05-19 10:21:02 -0400

    init project

---
+++ script.js
@@ -0,0 +1,22 @@
+// const button = document.getElementById("buttonSend")
+
+// button.addEventListener("click", function() {
+//     console.log("Hello World")
+// })
+
+
+function showMessage() {
+    window.location.href = "test.html"
+}
+
+
+// const a = showMessage = () => {
+//     console.log("Hello World")
+// }
+
+// a()
+
+const a = () => console.log("Hello World")
+
+
+a()
